d8a6415137ab227741bbf0f333d3b8eec4096bff
abmohamm <mohd.92java@gmail.com>
2020-12-10 13:55:09 -0500

    Created Students List, Student Details Components

--- src/app/create-student/create-student.component.ts
+++ src/app/create-student/create-student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Student } from '../Student';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-create-student',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateStudentComponent implements OnInit {
 
-  constructor() { }
+  student: Student = new Student();
+  submitted = false;
+  isAddressEnabled = false;
 
-  ngOnInit(): void {
+  constructor( private studentService: StudentService,
+               private router: Router ) { }
+
+  ngOnInit(): void { }
+
+  newStudent() {
+    this.submitted = false;
+    this.student = new Student();
   }
 
+  saveStudent() {
+    this.studentService.createStudent(this.student)
+                       .subscribe(data => {
+                          console.log('Student Created : ' + JSON.stringify(this.student));
+                          this.student = new Student();
+                          this.studentsList();
+                       })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.saveStudent();
+  }
+
+  studentsList() {
+    this.router.navigate(['students']);
+  }
+
+  addressLocationEnabled() {
+    this.isAddressEnabled = true;
+  }
 }
